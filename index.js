2c3f721927ff7174b824d7ae8ff701180d66d7e0
aberg07 <140882154+aberg07@users.noreply.github.com>
2024-02-19 08:02:47 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 require('dotenv').config();
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path}`);
+  next();
+})
+let currentUrl;
+
+const urlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true
+  },
+  short_url: {
+    type: String,
+    required: true
+  }
+});
+let url = mongoose.model('url', urlSchema);
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -15,8 +36,48 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', async (req, res) => {
+  let search = await url.findOne({original_url: req.body.url});
+  //If entry exists, return it from the database
+  if (search)
+  {
+    currentUrl = search;
+    res.json({
+      original_url: currentUrl.original_url,
+      short_url: currentUrl.short_url });
+    console.log("test");
+  }
+  else { //Otherwise create new entry in database
+    currentUrl = new url({
+      original_url: req.body.url,
+      short_url: Math.floor(Math.random() * 1000)
+    })
+    //If the random function happens to return an already existing short_url, generate another until a unique one is created
+    while (await url.findOne({short_url: currentUrl.short_url})) {
+      currentUrl.short_url = Math.floor(Math.random() * 1000);
+    }
+    await currentUrl.save();
+    console.log(currentUrl);
+    res.json({
+      original_url: currentUrl.original_url,
+      short_url: currentUrl.short_url 
+    });
+    console.log("test");
+  }
+})
+app.get('/api/shorturl', (req, res) => {
+  res.json({ original_url: currentUrl.original_url, short_url: currentUrl.short_url });
+});
+
+app.route('/api/shorturl/:short_url').get(async (req, res) => {
+  shortUrl = req.params.short_url;
+  redirectUrl = await url.findOne({short_url: shortUrl})
+  if (redirectUrl) {
+    res.redirect(redirectUrl.original_url);
+  }
+  else {
+    res.json({"error": "invalid url"})
+  }
 });
 
 app.listen(port, function() {
